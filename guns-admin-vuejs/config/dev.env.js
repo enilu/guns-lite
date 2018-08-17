a2bd4726e58c474c2892ddd30e1a57e31663bc25
enilu <82552623@qq.com>
2018-08-17 20:23:29 +0800

    add guns-admin-vuejs

---
+++ guns-admin-vuejs/config/dev.env.js
@@ -0,0 +1,8 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  BASE_API: '"https://easy-mock.com/mock/5ac8dac39724db4f80cc8733"',
+})
---
+++ guns-admin-vuejs/src/router/index.js
@@ -0,0 +1,146 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+// in development-env not use lazy-loading, because lazy-loading too many pages will cause webpack hot update too slow. so only in production use lazy-loading;
+// detail: https://panjiachen.github.io/vue-element-admin-site/#/lazy-loading
+
+Vue.use(Router)
+
+/* Layout */
+import Layout from '../views/layout/Layout'
+
+/**
+* hidden: true                   if `hidden:true` will not show in the sidebar(default is false)
+* alwaysShow: true               if set true, will always show the root menu, whatever its child routes length
+*                                if not set alwaysShow, only more than one route under the children
+*                                it will becomes nested mode, otherwise not show the root menu
+* redirect: noredirect           if `redirect:noredirect` will no redirct in the breadcrumb
+* name:'router-name'             the name is used by <keep-alive> (must set!!!)
+* meta : {
+    title: 'title'               the name show in submenu and breadcrumb (recommend set)
+    icon: 'svg-name'             the icon show in the sidebar,
+  }
+**/
+export const constantRouterMap = [
+  { path: '/login', component: () => import('@/views/login/index'), hidden: true },
+  { path: '/404', component: () => import('@/views/404'), hidden: true },
+
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    name: 'Dashboard',
+    hidden: true,
+    children: [{
+      path: 'dashboard',
+      component: () => import('@/views/dashboard/index')
+    }]
+  },
+
+
+  {
+    path:'/notice',
+    component:Layout,
+    children: [
+      {
+        path: 'index',
+        name: '通知',
+        component: () => import('@/views/notice/index'),
+        meta: { title: '通知', icon: 'eye' }
+      }
+    ]
+  },
+
+  {
+    path: '/nested',
+    component: Layout,
+    redirect: '/nested/menu1',
+    name: '系统管理',
+    meta: {
+      title: '系统管理',
+      icon: 'table'
+    },
+    children: [
+      {
+        path: 'menuMgr',
+        component: () => import('@/views/nested/menu2/index'),
+        meta: { title: '菜单管理' }
+      },
+      {
+        path: 'deptMgr',
+        component: () => import('@/views/nested/menu2/index'),
+        meta: { title: '部门管理' }
+      },
+      {
+        path: 'userMgr',
+          component: () => import('@/views/nested/menu2/index'),
+        meta: { title: '用户管理' }
+      },
+        {
+          path: 'roleMgr',
+            component: () => import('@/views/nested/menu2/index'),
+          meta: { title: '角色管理' }
+        },
+      {
+        path: 'dictMgr',
+          component: () => import('@/views/nested/menu2/index'),
+        meta: { title: '字典管理' }
+      },
+      {
+        path: 'loginLog',
+          component: () => import('@/views/nested/menu2/index'),
+        meta: { title: '登录日志' }
+      },
+      {
+        path: 'businessLog',
+          component: () => import('@/views/nested/menu2/index'),
+        meta: { title: '业务日志' }
+      },
+      {
+        path: 'monitor',
+          component: () => import('@/views/nested/menu2/index'),
+        meta: { title: '监控管理' }
+      },
+      {
+        path: 'noticeMgr',
+          component: () => import('@/views/nested/menu2/index'),
+        meta: { title: '通知管理' }
+      },
+      {
+        path: 'sysCfg',
+          component: () => import('@/views/nested/menu2/index'),
+        meta: { title: '参数管理' }
+      }
+  ]
+},
+
+  {
+    path: '/example',
+      component: Layout,
+    redirect: '/example/table',
+    name: 'Example',
+    meta: { title: 'Example', icon: 'example' },
+    children: [
+        {
+          path: 'table',
+          name: 'Table',
+          component: () => import('@/views/table/index'),
+          meta: { title: 'Table', icon: 'table' }
+      },
+      {
+        path: 'tree',
+          name: 'Tree',
+        component: () => import('@/views/tree/index'),
+        meta: { title: 'Tree', icon: 'tree' }
+      }
+    ]
+  },
+  { path: '*', redirect: '/404', hidden: true }
+]
+
+export default new Router({
+  // mode: 'history', //后端支持可开
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRouterMap
+})
+
