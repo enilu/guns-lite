2287eb4757952e52170bb42221442d69a373d74e
enilu <eniluzt@qq.com>
2019-05-07 12:21:01 +0800

    add doc

--- guns-entity/src/main/java/cn/enilu/guns/bean/constant/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created  on 2018/3/28 0028.
- *
- * @author enilu
- */
-package cn.enilu.guns.bean.constant;
--- guns-service/src/main/java/cn/enilu/guns/service/task/NoConurrentBaseJob.java
+++
@@ -1,9 +0,0 @@
-package cn.enilu.guns.service.task;
-
-import org.quartz.DisallowConcurrentExecution;
-import org.springframework.stereotype.Component;
-
-@Component
-@DisallowConcurrentExecution
-public class NoConurrentBaseJob extends BaseJob {
-}
