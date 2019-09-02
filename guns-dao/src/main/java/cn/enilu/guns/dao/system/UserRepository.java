a761b174d86e6132ca3a9d9712d3ad34c6ccd11f
enilu <eniluzt@qq.com>
2019-09-02 19:10:42 +0800

    Change 删除角色时判断是有用户使用该角色，如果有则不允许删除

--- guns-dao/src/main/java/cn/enilu/guns/dao/system/UserRepository.java
+++ guns-dao/src/main/java/cn/enilu/guns/dao/system/UserRepository.java
@@ -4,6 +4,8 @@ package cn.enilu.guns.dao.system;
 import cn.enilu.guns.bean.entity.system.User;
 import cn.enilu.guns.dao.BaseRepository;
 
+import java.util.List;
+
 /**
  * Created  on 2018/3/21 0021.
  *
@@ -11,4 +13,6 @@ import cn.enilu.guns.dao.BaseRepository;
  */
 public interface UserRepository extends BaseRepository<User,Long> {
     User findByAccount(String account);
+
+    List<User> findByRoleid(String valueOf);
 }
